30c29bbb1ce003cafe75c4fcd8bbbbcd343d1a19
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 19:10:53 +0700

    refactor: Change module name to base; change class name from Base to BaseModel

--- main/models/base.py
+++ main/models/base.py
@@ -4,10 +4,10 @@ from sqlalchemy import Column, DateTime, Integer
 from sqlalchemy.ext.declarative import declared_attr, declarative_base
 
 
-class Base:
+class BaseModel:
     id = Column(Integer, primary_key=True, index=True)
     created = Column(DateTime, default=datetime.now)
     updated = Column(DateTime)
 
 
-Base = declarative_base(cls=Base)
+Base = declarative_base(cls=BaseModel)
