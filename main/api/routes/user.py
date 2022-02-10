324eae0bd63a7cde73f82d78ae856a7402ed88a7
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 16:18:41 +0700

    refactor: Change to use AsyncSession; Follow naming convention so that code looks more explicit

--- main/api/routes/user.py
+++ main/api/routes/user.py
@@ -2,26 +2,22 @@ from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from main.api.dependencies.get_db import get_db_session
-from main.models.user import User
-from main.schemas.user import (
-    UserAuthenticationRequestSchema,
-    UserCreationRequestSchema
-)
-from main.schemas.token import AccessToken
+from main.api.dependencies.get_database import get_database_session
+from main.models.user import UserModel
+from main.schemas.auth import AccessToken
+from main.schemas.user import UserAuthenticationRequestSchema, UserCreationRequestSchema
 from main.services.auth import create_access_token, verify_password
-from main.services.database.user.crud import create_user, get_user_by_email
+from main.services.auth import generate_hashed_password
+from main.services.database.user import create_user, get_user_by_email
 
 router = APIRouter()
 
 
-def authenticate_user(
-        db: Session, user_login: UserAuthenticationRequestSchema
-) -> Optional[User]:
-    user = get_user_by_email(db, user_login.email)
-    if not user:
+async def authenticate_user(session: AsyncSession, user_login: UserAuthenticationRequestSchema) -> Optional[UserModel]:
+    user = await get_user_by_email(session, user_login.email)
+    if user is None:
         return None
 
     if not verify_password(user_login.password, user.hashed_password):
@@ -31,33 +27,31 @@ def authenticate_user(
 
 
 @router.post("/")
-async def register(
-        create_user_data: UserCreationRequestSchema,
-        db: Session = Depends(get_db_session)
-):
-    user = get_user_by_email(db, create_user_data.email)
+async def register(create_user_data: UserCreationRequestSchema, session: AsyncSession = Depends(get_database_session)):
+    user = await get_user_by_email(session, create_user_data.email)
     if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="This email is already registered"
+            detail="This email is already registered",
         )
-
-    create_user(db, create_user_data)
+    hashed_password = generate_hashed_password(create_user_data.password)
+    await create_user(
+        session=session,
+        email=create_user_data.email,
+        hashed_password=hashed_password,
+        full_name=create_user_data.full_name,
+    )
     return JSONResponse(content={}, status_code=status.HTTP_201_CREATED)
 
 
 @router.post("/auth", response_model=AccessToken)
-async def auth(
-        user_login: UserAuthenticationRequestSchema,
-        db: Session = Depends(get_db_session)
+async def authenticate(
+    user_authentication_data: UserAuthenticationRequestSchema,
+    session: AsyncSession = Depends(get_database_session),
 ):
-    user = authenticate_user(db, user_login)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid email or password"
-        )
+    user = await authenticate_user(session, user_authentication_data)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
 
     access_token = create_access_token(user.id)
-    return JSONResponse(content={"access_token": access_token},
-                        status_code=status.HTTP_200_OK)
+    return JSONResponse(content={"access_token": access_token}, status_code=status.HTTP_200_OK)
