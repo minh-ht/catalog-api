a0190d9e9d37d0997dde699f31bf22a497ddc155
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 19:27:31 +0700

    refactor: Correct pycharm's auto refactor

--- main/services/session.py
+++ main/services/session.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from main.config import settings
-from main.models.base import BaseModel
+from main.models.base import Base
 
 engine = create_async_engine(settings.SQL_ALCHEMY_DATABASE_URL, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
@@ -11,4 +11,4 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, clas
 async def create_table():
     async with engine.begin() as conn:
         # await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(BaseModel.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
