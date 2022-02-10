c34d2081f26675284c6ad47e0fe8728254796020
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 15:29:28 +0700

    style: Adapt to max line length 120

--- main/config.py
+++ main/config.py
@@ -5,12 +5,10 @@ class Settings(BaseSettings):
     ITEMS_PER_PAGE: int = 20
 
     # Database config
-    SQL_ALCHEMY_DATABASE_URL = "mysql+pymysql://" \
-                               "minhhoang:lighT023@localhost/catalog"
+    SQL_ALCHEMY_DATABASE_URL = "mysql+aiomysql://minhhoang:lighT023@localhost/catalog"
 
     # Security config
-    JWT_SECRET_KEY = "4ca3722e7e3649a19fdea8c1ac2db5" \
-                 "ebb7bb59b8d5c49a664329239d42f6acea"
+    JWT_SECRET_KEY = "4ca3722e7e3649a19fdea8c1ac2db5ebb7bb59b8d5c49a664329239d42f6acea"
     ALGORITHM = "HS256"
     JWT_EXPIRED_MINUTES = 30
 
