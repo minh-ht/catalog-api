2852137b0027278f69a808ecb3970701b4e10b0d
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 16:51:06 +0700

    feat: Add dependency to get category from path parameter

---
+++ main/api/dependencies/category.py
@@ -0,0 +1,14 @@
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from main.api.dependencies.get_database import get_database_session
+from main.models.category import CategoryModel
+from main.services.database import category as category_service
+
+
+async def get_category_by_id(category_id: int, session: AsyncSession = Depends(get_database_session)) -> CategoryModel:
+    category = await category_service.get_category_by_id(session, category_id)
+    if category is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Cannot find the specified category")
+    return category
