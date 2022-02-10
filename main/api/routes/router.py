eea3ef643572e4fb4d44c9965e6c1d7e5ac384ec
squirrelwine <squirrelwhisky@gmail.com>
2022-02-11 01:07:59 +0700

    refactor: Delete unrelated branch commit

--- main/api/routes/router.py
+++ main/api/routes/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from main.api.routes import category, user
+from main.api.routes import user
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/users", tags=["users"])
-api_router.include_router(category.router, prefix="/categories", tags=["categories"])
