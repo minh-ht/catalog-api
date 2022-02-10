5a61ac6caa2cf1ba2d62b9c3bf55fa9f32f488ca
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 15:21:13 +0700

    refactor: Change file name from api.py to router.py and put into route

--- main/api/routes/router.py
+++ main/api/routes/router.py
@@ -3,6 +3,5 @@ from fastapi import APIRouter
 from main.api.routes import category, user
 
 api_router = APIRouter()
-api_router.include_router(user.router,
-                          prefix="/users",
-                          tags=["users"])
+api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(category.router, prefix="/categories", tags=["categories"])
