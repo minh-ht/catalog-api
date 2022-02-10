d68041b20af002f08622e0bfacbf60e514853b00
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 17:01:22 +0700

    refactor: Change to use AsyncSession; Add dependency to check for user permission

--- main/api/routes/category.py
+++ main/api/routes/category.py
@@ -1,79 +1,60 @@
-from typing import List, Optional
+from typing import List
 
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from main.api.dependencies.auth import get_current_user_id
-from main.api.dependencies.get_db import get_db_session
+from main.api.dependencies.auth import require_authenticated_user, require_permission_on_category
+from main.api.dependencies.get_database import get_database_session
+from main.common.exception import NoEntityError
+from main.models.user import UserModel
 from main.schemas.category import CategoryBatchResponseSchema, CategoryCreationRequestSchema, CategoryResponseSchema
-from main.services.database import category_crud
+from main.services.database import category as category_service
 
 router = APIRouter()
 
 
 @router.get("/", response_model=List[CategoryBatchResponseSchema], status_code=status.HTTP_200_OK)
-async def get_all_categories(session: Session = Depends(get_db_session)):
-    categories = category_crud.get_categories(session)
+async def get_all_categories(session: AsyncSession = Depends(get_database_session)):
+    categories = await category_service.get_categories(session)
     return categories
 
 
 @router.get("/{category_id}", response_model=CategoryResponseSchema, status_code=status.HTTP_200_OK)
-async def get_single_category(category_id, session: Session = Depends(get_db_session)):
-    category = category_crud.get_category_by_id(session, category_id)
-    if category is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Cannot find the specified category"
-        )
+async def get_single_category(category_id: int, session: AsyncSession = Depends(get_database_session)):
+    category = await category_service.get_category_by_id(session, category_id)
     return category
 
 
 @router.post("/")
 async def create_category(
     create_category_data: CategoryCreationRequestSchema,
-    session: Session = Depends(get_db_session),
-    user_id: Optional[int] = Depends(get_current_user_id)
+    session: AsyncSession = Depends(get_database_session),
+    user: UserModel = Depends(require_authenticated_user),
 ):
-    if user_id is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User needs to authenticate to create new category"
-        )
-    if category_crud.get_category_by_name(session, create_category_data.name):
+    category = await category_service.get_category_by_name(session, create_category_data.name)
+    if category:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Category name already exists"
+            detail="Category name already exists",
         )
-    category_crud.create_category(session, create_category_data.name, create_category_data.description, user_id)
+    await category_service.create_category(
+        session=session,
+        name=create_category_data.name,
+        description=create_category_data.description,
+        user_id=user.id,
+    )
     return JSONResponse(content={}, status_code=status.HTTP_201_CREATED)
 
 
-@router.delete("/{category_id}")
-async def delete_category(
-    category_id,
-    session: Session = Depends(get_db_session),
-    user_id: Optional[int] = Depends(get_current_user_id)
-):
-    if user_id is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User needs to authenticate to delete category"
-        )
-
-    category = category_crud.get_category_by_id(session, category_id)
-    if category is None:
+@router.delete("/{category_id}", dependencies=[Depends(require_permission_on_category)])
+async def delete_category(category_id, session: AsyncSession = Depends(get_database_session)):
+    try:
+        await category_service.delete_category(session, category_id)
+    except NoEntityError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Cannot delete the specified category"
+            detail="Cannot delete the specified category",
         )
-
-    if user_id != category.user_id:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="User does not have permission to delete this category"
-        )
-
-    category.delete_category(session, category)
     return JSONResponse(content={}, status_code=status.HTTP_200_OK)
