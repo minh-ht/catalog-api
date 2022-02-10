ccbfcd2e20178fa10443549a43affafd678e673c
squirrelwine <squirrelwhisky@gmail.com>
2022-02-10 19:30:02 +0700

    refactor: Make use of sqlalchemy.sql.func to auto update time

--- main/models/base.py
+++ main/models/base.py
@@ -1,13 +1,12 @@
-from datetime import datetime
-
 from sqlalchemy import Column, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
 
 class BaseModel:
     id = Column(Integer, primary_key=True, index=True)
-    created = Column(DateTime, default=datetime.now)
-    updated = Column(DateTime)
+    created = Column(DateTime, server_default=func.now())
+    updated = Column(DateTime, onupdate=func.now())
 
 
 Base = declarative_base(cls=BaseModel)
