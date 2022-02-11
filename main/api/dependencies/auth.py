e5da4c79c89c58d9a7dba9774e8f9a5b93653119
squirrelwine <squirrelwhisky@gmail.com>
2022-02-11 17:47:46 +0700

    feat: Add require_permission_on_item

--- main/api/dependencies/auth.py
+++ main/api/dependencies/auth.py
@@ -1,13 +1,15 @@
 from fastapi import Depends
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from jose import JWTError, jwt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from main.api.dependencies.category import get_category_by_id
 from main.api.dependencies.database import get_database_session
+from main.api.dependencies.item import get_item_by_id
 from main.common.exception import ForbiddenException, UnauthorizedException
 from main.config import settings
 from main.models.category import CategoryModel
+from main.models.item import ItemModel
 from main.models.user import UserModel
 from main.services.user import get_user_by_id
 
@@ -34,7 +36,16 @@ async def require_authenticated_user(
 
 
 async def require_permission_on_category(
-    user: UserModel = Depends(require_authenticated_user), category: CategoryModel = Depends(get_category_by_id)
+    user: UserModel = Depends(require_authenticated_user),
+    category: CategoryModel = Depends(get_category_by_id),
 ) -> None:
     if category.user_id != user.id:
+        raise ForbiddenException("User does not have permission to perform this action")
+
+
+async def require_permission_on_item(
+    user: UserModel = Depends(require_authenticated_user),
+    item: ItemModel = Depends(get_item_by_id),
+) -> None:
+    if item.user_id != user.id:
         raise ForbiddenException("User does not have permission to perform this action")
