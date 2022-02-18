f18c56fe1ddaf9d029671b9ef7306bf865f37c5d
squirrelwine <squirrelwhisky@gmail.com>
2022-02-18 15:33:34 +0700

    refactor: Remove unused config (error_msg_templates)

--- main/schemas/base.py
+++ main/schemas/base.py
@@ -6,6 +6,3 @@ class BaseSchema(BaseModel):
         orm_mode = True
         anystr_strip_whitespace = True
         extra = Extra.forbid
-        error_msg_templates = {
-            "value_error.any_str.max_length": " exceeds max length ({limit_value})",
-        }
